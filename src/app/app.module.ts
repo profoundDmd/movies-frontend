884cfb3fb1a6c21b4c64de1a079ac1969d3848c8
profoundDmd <jadrandub@gmail.com>
2023-05-22 22:05:03 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './component/add-movie/add-movie/add-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/service/Movie.ts
@@ -0,0 +1,6 @@
+export class Movie {
+  id!: number;
+  title!: String;
+  description!: String;
+  year!: Number;
+}
