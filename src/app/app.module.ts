9e76f27b374f17eecc52a0c38ac49c7ef97574f6
profoundDmd <jadrandub@gmail.com>
2023-05-22 22:19:54 +0200

    Small refactor and added addmovie-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AddMovieComponent } from './component/add-movie/add-movie/add-movie.component';
+import { AddMovieComponent } from './component/add-movie/add-movie.component';
+import { MovieDetailComponent } from './component/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddMovieComponent
+    AddMovieComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
